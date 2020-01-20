5f17abddc56e6a4281e10b6e861cb3d8b6452803
kcuyh <khj879123@gmail.com>
2020-01-20 09:34:25 +0900

    [update] line

--- lib/Page/HomePage.dart
+++ lib/Page/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttersampleui/setting.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -7,26 +8,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Column(
         children: <Widget>[
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Icon(
                 Icons.camera_alt,
-                color: Colors.black,
+                color: IconColors,
               ),
               Text(
                 'Instagram',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).textScaleFactor*25,
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w500
+                ),
               ),
               Icon(
-                Icons.camera_alt,
-                color: Colors.black,
+                Icons.send,
+                color: IconColors,
               ),
             ],
           ),
+          Container(
+            width: MediaQuery.of(context).size.width/2,
+            height: 0.1,
+            color: Colors.white,
+          ),
+
         ],
       ),
     );
